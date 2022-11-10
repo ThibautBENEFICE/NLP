8e6650d13e65e69cb38b3aad8e94ff540120851f
victor.litoux <victor.litoux@epita.fr>
2022-11-10 15:13:00 +0100

    Fix: write on dict

--- lab_07/load_dataset.py
+++ lab_07/load_dataset.py
@@ -1,3 +1,4 @@
+import itertools
 import os.path
 
 import beir
@@ -18,25 +19,20 @@ def download_dataset(folder_path: str):
 	corpus, queries, qrels = GenericDataLoader(data_folder=data_path).load(split="test")
 	return corpus, queries, qrels
 
-
-def create_smaller_corpus(corpus: dict, queries, qrels: dict, folder_path: str, number_themes: int):
-	set_usefull_themes = set()
-	for q in qrels:
-		for t in qrels[q]:
-			set_usefull_themes.add(t)
-	obj = set([el for el in tqdm.tqdm(corpus)])
-	obj = obj.difference(set_usefull_themes)
-	trash_themes = random.sample(obj, k=number_themes)
-	set_usefull_themes = set_usefull_themes.union(trash_themes)
-	corpus2 = {key: value for (key, value) in corpus.items() if key in set_usefull_themes}
+def create_smaller_corpus(corpus: dict, qrels: dict, folder_path: str, number_thrash: int):
+	usefull_documents_title = list(
+		dict.fromkeys(itertools.chain.from_iterable(map(lambda x: list(x.keys()), qrels.values()))))
+	useless_documents_title = list(set(corpus.keys()) - set(usefull_documents_title))
+	final_corpus_titles = random.sample(useless_documents_title, number_thrash) + usefull_documents_title
+	final_corpus_titles = list(filter(lambda x: x in corpus.keys(), final_corpus_titles))
+	simple_corpus = {k: corpus[k] for k in final_corpus_titles}
 	list_json = []
-	print("Create list_json")
-	for el in tqdm.tqdm(corpus2):
-		list_json.append(
-			{"_id": el, "title": el[9:-1], "text": corpus2[el],
-				"metadata": f"<http://dbpedia.org/resource/{el[9:-1]}>"})
+	for el in tqdm.tqdm(simple_corpus):
+		list_json.append({el: simple_corpus[el]})
 	print("Saving corpus")
 	with open(os.path.join(folder_path, "dbpedia-entity", "corpus.jsonl"), "w") as file:
 		for item in tqdm.tqdm(list_json):
 			file.write(json.dumps(item) + "\n")
-	return corpus2
+	return simple_corpus
+
+
